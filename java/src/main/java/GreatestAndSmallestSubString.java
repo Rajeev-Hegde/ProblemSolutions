1f9e13a57d86f319a7e562edf9a6ec3c5f7e9486
Rajeev-Hegde <rajeevh.93@gmail.com>
2018-03-02 18:59:12 +0530

    Greatest And Smallest Substring of length K from a given string

---
+++ java/src/main/java/GreatestAndSmallestSubString.java
@@ -0,0 +1,32 @@
+
+
+
+
+public class GreatestAndSmallestSubString {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        smallest=s.substring(0,k);
+        largest=s.substring(0,k);
+        for(int i=0;i<s.length()-k+1;i++){
+            if(smallest.compareTo(s.substring(i,i+k))>0)
+                smallest=s.substring(i,i+k);
+            else if(largest.compareTo(s.substring(i,i+k))<0)
+                largest=s.substring(i,i+k);
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "welcometojava";
+        int k = 3;
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
