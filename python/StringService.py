6633d998c88fefd37597b18c8f45fbb5f427ff56
Rajeev-Hegde <rajeevh.93@gmail.com>
2018-10-14 22:16:40 +0530

    Print unique chars in substring

---
+++ python/StringService.py
@@ -0,0 +1,11 @@
+
+
+def print_unique_chars_in_sub_string(str,k):
+    for item in str:
+        len_temp += 1
+        if item not in temp:
+            temp.append(item)
+        if len_temp == k:
+            print ''.join(temp)
+            temp = []
+            len_temp = 0
