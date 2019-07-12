419b7166e06a35c0a11c6b5349eaf5b0cb3833c7
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-07-12 11:09:38 +0530

    Counting valleys

---
+++ java/src/main/java/algorithms/CountingValleys.java
@@ -0,0 +1,42 @@
+package algorithms;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class CountingValleys {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        int count=0;
+        int num=0;
+        for (char ch: s.toCharArray()){
+            if(ch == 'D') {
+                count--;
+                if(count == -1)
+                    num++;
+            }
+            else
+                count++;
+        }
+        return num;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        System.out.println(result);
+        scanner.close();
+    }
+}
