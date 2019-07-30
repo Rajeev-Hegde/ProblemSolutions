58e5bd89e5cca02c43972ac61457d491de9963c0
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-07-30 20:34:27 +0530

    Allocate books

---
+++ java/src/main/java/problems/interviewbit/binarysearch/AllocateBooks.java
@@ -0,0 +1,14 @@
+package problems.interviewbit.binarysearch;
+
+import java.util.ArrayList;
+
+public class AllocateBooks {
+
+    public int books(ArrayList<Integer> A, int B) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
