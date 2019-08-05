bb4d9198543668c0aa424c2565d28ce24fd6c5ea
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-08-06 00:19:38 +0530

    Invert Binary Tree

---
+++ java/src/main/java/problems/interviewbit/mock_interviews/InvertBinaryTree.java
@@ -0,0 +1,75 @@
+package problems.interviewbit.mock_interviews;
+
+import com.google.gson.Gson;
+
+public class InvertBinaryTree {
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+            left = null;
+            right = null;
+        }
+    }
+
+
+    public void swap(TreeNode A){
+        TreeNode temp=A.left;
+        A.left=A.right;
+        A.right = temp;
+    }
+
+    public TreeNode invertTree(TreeNode A) {
+
+        if(A.left==null && A.right== null)
+            return null;
+
+        if(A.left!=null)
+            invertTree(A.left);
+
+        if(A.right!=null)
+            invertTree(A.right);
+
+        swap(A);
+        return A;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(3);
+        treeNode.left=  new TreeNode(1);
+        treeNode.right=  new TreeNode(-1);
+
+        treeNode.left.left=  new TreeNode(-1);
+//        treeNode.left.right=  new TreeNode(5);
+//
+//        treeNode.right.left=  new TreeNode(6);
+//        treeNode.right.right=  new TreeNode(7);
+
+        System.out.println(new Gson().toJson(new InvertBinaryTree().invertTree(treeNode)));
+    }
+
+    /*
+    SWAPS first and then goes inside. Try this solution if above solution dosen't work
+    public TreeNode invertTree(TreeNode A) {
+        if (A == null) return A;
+        swap(A);
+        return A;
+    }
+
+    public void swap(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return;
+        }
+        TreeNode n1 = root.left;
+        root.left = root.right;
+        root.right = n1;
+
+        if (root.left != null) swap(root.left);
+        if (root.right != null) swap(root.right);
+    }*/
+}
