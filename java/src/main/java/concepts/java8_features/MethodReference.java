86e96ee1649ff81e44f82de90595fa803abfe9de
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-08-06 22:56:25 +0530

    compose functionality added

--- java/src/main/java/concepts/java8_features/MethodReference.java
+++ java/src/main/java/concepts/java8_features/MethodReference.java
@@ -3,6 +3,8 @@ package concepts.java8_features;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class MethodReference {
 
@@ -23,6 +25,14 @@ public class MethodReference {
         return Arrays.stream(numbers).max(Comparator.naturalOrder()).get();
     }
 
+    private static Integer calculateMinInArray(Integer[] numbers){
+        return Arrays.stream(numbers).min(Comparator.naturalOrder()).get();
+    }
+
+    private static Integer[] removeMinusOne(Integer[] numbers){
+        return Arrays.stream(numbers).filter(num-> num!=-1).toArray(Integer[]::new);
+    }
+
     public static void main(String[] args) {
         Print print= MethodReference::staticWelcomeMessage;
         print.print("Hello world from static method");
@@ -32,8 +42,13 @@ public class MethodReference {
         print1.print("Hello world from object method");
 
 
-        Function<Integer[],Integer> function = MethodReference::calculateMaxInArray;
-        System.out.println(function.apply(new Integer[]{2,7,3,5}));
+        Function<Integer[],Integer> functionMax = MethodReference::calculateMaxInArray;
+        Function<Integer[],Integer> functionMin = MethodReference::calculateMinInArray;
+        Function<Integer[],Integer[]> removeMinusOne = MethodReference::removeMinusOne;
+        System.out.println(functionMax.apply(new Integer[]{2,7,3,5,-1, -1}));
+
+        //Removes -1 from array before finding out minimum value
+        System.out.println(functionMin.compose(removeMinusOne).apply(new Integer[]{2,7,3,5,-1, -1}));
 
     }
 }
