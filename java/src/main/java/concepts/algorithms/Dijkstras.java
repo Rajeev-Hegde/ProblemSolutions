2b637d62b51c39d0eff3d9056548eb93fedc9670
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-08-14 14:39:16 +0530

    1. Fibinacci => Dynamic programming technique             2. dijkstras

--- java/src/main/java/concepts/algorithms/Dijkstras.java
+++ java/src/main/java/concepts/algorithms/Dijkstras.java
@@ -7,16 +7,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Dijkstras {
 
 
-    int getMinDistanceVertex(Map<Integer, Integer> vertexVsShortestDistanceFromSource, Boolean[] visitedVertices) {
+    private int getMinDistanceVertex(Map<Integer, Integer> vertexVsShortestDistanceFromSource, Boolean[] visitedVertices) {
         AtomicInteger min =  new AtomicInteger(Integer.MAX_VALUE);
-        int vertexIndex=-1;
+        AtomicInteger vertexIndex= new AtomicInteger( -1);
         vertexVsShortestDistanceFromSource.forEach((key,val)-> {
-            if(!visitedVertices[key]&& val <= min.get()){
+            if(!visitedVertices[key] && val < min.get()){
                 min.set(vertexVsShortestDistanceFromSource.get(key));
+                vertexIndex.set(key);
             }
         });
 
-        return vertexIndex;
+        return vertexIndex.get();
     }
 
     public Map<Integer, Integer> getVertexVsShortestDistanceFromSource(int graph[][], Integer sourceVertex, Integer totalVertices) {
@@ -30,6 +31,7 @@ public class Dijkstras {
         }
 
         vertexVsShortestDistanceFromSource.put(sourceVertex, 0);
+        //visitedVertices[sourceVertex]=true;
 
         for (int i = 0; i < totalVertices; i++) {
             int minDistanceVertex = getMinDistanceVertex(vertexVsShortestDistanceFromSource, visitedVertices);
@@ -53,6 +55,17 @@ public class Dijkstras {
     }
 
     public static void main(String[] args) {
-
+        int graph[][] = new int[][]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+        };
+        Dijkstras dijkstras = new Dijkstras();
+        System.out.println(dijkstras.getVertexVsShortestDistanceFromSource(graph, 0,9));
     }
 }
