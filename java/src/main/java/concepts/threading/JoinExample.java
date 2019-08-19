3f1931b62841bbfab581757f226f2eef4e3863ec
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-08-19 23:44:42 +0530

    Join example improved

--- java/src/main/java/concepts/threading/JoinExample.java
+++ java/src/main/java/concepts/threading/JoinExample.java
@@ -1,32 +1,51 @@
 package concepts.threading;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.stream.IntStream;
 
 public class JoinExample {
 
-
+    private static CustomTask customTask = (numTasks,delayMilliSecs)-> {
+        IntStream.range(1,numTasks+1).forEach(num-> {
+            System.out.println(Thread.currentThread().getName()+ " Task finished: "+ num);
+            try {
+                Thread.sleep(delayMilliSecs);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    };
 
 
     public interface CustomTask{
-        void execute(int numTasks);
+        void execute(int numTasks,int delay);
+
+        default void executeDefault(int numTasks){
+            IntStream.range(1,numTasks+1).forEach(num->
+                    System.out.println(Thread.currentThread().getName()+ " Task finished: "+ num));
+        }
     }
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        CustomTask customTask = (numTasks)-> {
-            IntStream.range(1,numTasks+1).forEach(num-> System.out.println(Thread.currentThread().getName()+ " Task finished: "+ num));
-        };
 
-        Thread thread1= new Thread(()-> customTask.execute(2));
-        Thread thread2=new Thread(()-> customTask.execute(6));
-        Thread thread3=new Thread(()-> customTask.execute(10000));
+        Thread thread1= new Thread(()-> customTask.execute(2,1000));
+        Thread thread2=new Thread(()-> customTask.execute(6,1000));
+        Thread thread3=new Thread(()-> customTask.executeDefault(10000));
+        FutureTask<String> futureTask = new FutureTask<>(() -> "This is a returned result from thread execution");
+        Thread thread4 = new Thread(futureTask);
 
         thread3.start();
         thread3.join();
         //thread1 and thread2 begins execution only after thread3 finishes execution
         thread1.start();
         thread2.start();
+        thread4.start();
+
+        System.out.println(futureTask.get());
 
         //thread1.join();
         //thread2.join();
