60a9861de9e7ba1ab916dbea26949c4a6eff7054
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-09-21 00:06:40 +0530

    Kth Smallest in BST

---
+++ java/src/main/java/problems/leetcode/medium/KthSmallestInBST.java
@@ -0,0 +1,25 @@
+package problems.leetcode.medium;
+
+import problems.interviewbit.trees.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KthSmallestInBST {
+
+
+    public void constructStack(TreeNode root, List<Integer> list){
+        if(root!= null){
+            if(root.left!=null)
+                constructStack(root.left, list);
+            list.add(root.val);
+            if(root.right!=null)
+                constructStack(root.right, list);
+        }
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        constructStack(root,list);
+        return list.get(k-1);
+    }
+}
