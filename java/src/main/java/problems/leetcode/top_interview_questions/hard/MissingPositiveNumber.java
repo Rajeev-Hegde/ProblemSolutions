a63e20829b07dfd75fd7d8be7118c44733c0e108
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-09-26 18:11:19 +0530

    Missing positive number Letter combination of phone number

--- java/src/main/java/problems/leetcode/top_interview_questions/hard/MissingPositiveNumber.java
+++ java/src/main/java/problems/leetcode/top_interview_questions/hard/MissingPositiveNumber.java
@@ -3,18 +3,13 @@ package problems.leetcode.top_interview_questions.hard;
 /**
  * Given an unsorted integer array, find the smallest missing positive integer.
  */
+
+/**
+ * TODO:// find solution for this
+ */
 public class MissingPositiveNumber {
 
     public int firstMissingPositive(int[] nums) {
-        if(nums==null || nums.length==0)
-            return -1;
-        int currentMin=nums[0], currentPos=0;
-
-        for(int i=1; i< nums.length; i++){
-            if((nums[i] & currentMin) < currentMin)
-                currentPos=i;
-            currentMin=nums[i] & currentMin;
-        }
-        return currentPos;
+        return 0;
     }
 }
