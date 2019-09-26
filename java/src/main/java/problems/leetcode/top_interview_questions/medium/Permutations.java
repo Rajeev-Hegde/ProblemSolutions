a649cf5632ecc5b4e7685d39bc2470b944214bcb
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-09-26 19:16:13 +0530

    Permutations for a given numbers array using swap method

--- java/src/main/java/problems/leetcode/top_interview_questions/medium/Permutations.java
+++ java/src/main/java/problems/leetcode/top_interview_questions/medium/Permutations.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations {
-    public List<List<Integer>> permute(int[] nums) {
+
+    //Simple recursive approach
+    public List<List<Integer>> permuteUsingSimpleRecursive(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         getPermutations(nums,res,new ArrayList<>(),new ArrayList<>());
         return res;
@@ -23,6 +25,36 @@ public class Permutations {
                 getPermutations(nums,res,temp, tempCoveredIndices);
             }
         }
+    }
+
+    //Recursive +swap approach
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return new ArrayList<>();
+
+        List<List<Integer>> lists = new ArrayList<>();
+        backtrack(lists, new ArrayList<>(), nums, 0);
+        return lists;
+    }
+
+    private void backtrack(List<List<Integer>> lists, List<Integer> list, int[] nums, int pos) {
+        if (pos == nums.length) {
+            lists.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = pos; i < nums.length; i++) {
+            swap(nums, pos, i);
+            list.add(nums[pos]);
+            backtrack(lists, list, nums, pos + 1);
+            swap(nums, pos, i);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     public static void main(String[] args) {
