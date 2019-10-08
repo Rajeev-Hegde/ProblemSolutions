7e2fdc0d3e07653f531e59a9e6c5daa8879abfb6
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-10-08 16:26:42 +0530

    Num Of unique emails

--- java/src/main/java/problems/leetcode/top_interview_questions/easy/NumOfUniqueEmails.java
+++ java/src/main/java/problems/leetcode/top_interview_questions/easy/NumOfUniqueEmails.java
@@ -15,4 +15,24 @@ public class NumOfUniqueEmails {
         return uniqueEmails.size();
 
     }
+
+    public int numUniqueEmailsSolution2(String[] emails) {
+        int counter = 0;
+        Set<String> emailSet = new HashSet<>();
+
+        for (String s : emails) {
+            int index = -1;
+            StringBuilder sb = new StringBuilder();
+
+            while (s.charAt(++index) != '@') {
+                if (s.charAt(index) == '.') continue;
+                else if (s.charAt(index) == '+') break;
+                else sb.append(s.charAt(index));
+            }
+
+            if (emailSet.add(sb.append('@').toString() + s.substring(s.indexOf('@') + 1))) counter++;
+        }
+
+        return counter;
+    }
 }
