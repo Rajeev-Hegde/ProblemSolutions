f457b66c6f53b44cd8fa1e1bd60e894503fea23b
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-10-08 17:56:36 +0530

    Logger rate limiter

--- java/src/main/java/problems/leetcode/top_interview_questions/easy/LoggerRateLimiter.java
+++ java/src/main/java/problems/leetcode/top_interview_questions/easy/LoggerRateLimiter.java
@@ -30,4 +30,12 @@ public class LoggerRateLimiter {
         }
 
     }
+
+    private Map<String, Integer> ok = new HashMap<>();
+    public boolean shouldPrintMessageFasterApproach(int timestamp, String message) {
+        if (timestamp < ok.getOrDefault(message, 0))
+            return false;
+        ok.put(message, timestamp + 10);
+        return true;
+    }
 }
