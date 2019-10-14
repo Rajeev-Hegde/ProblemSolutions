17fa7c0ccc92e9e2848a5d4513099c84e615facb
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-10-14 21:06:59 +0530

    iterative method

--- java/src/main/java/problems/leetcode/top_interview_questions/easy/SymmetricTree.java
+++ java/src/main/java/problems/leetcode/top_interview_questions/easy/SymmetricTree.java
@@ -2,6 +2,9 @@ package problems.leetcode.top_interview_questions.easy;
 
 import problems.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * IMPORTANT
  */
@@ -16,5 +19,26 @@ public class SymmetricTree {
         if(p==null && q==null) return true;
         if(p==null || q==null || p.val!=q.val)return false;
         return isSame(p.right,q.left) && isSame(p.left,q.right);
+    }
+
+
+    public boolean isSymmetricRecursive(TreeNode root) {
+        if(root == null) return true;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+
+        while(!q.isEmpty()){
+            TreeNode leftSub = q.poll();
+            TreeNode rightSub = q.poll();
+            if(leftSub == null && rightSub == null) continue;
+            if(leftSub == null || rightSub == null) return false;
+            if(leftSub.val != rightSub.val) return false;
+            q.offer(leftSub.left);
+            q.offer(rightSub.right);
+            q.offer(leftSub.right);
+            q.offer(rightSub.left);
+        }
+        return true;
     }
 }
