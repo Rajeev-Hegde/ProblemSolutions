39b2321357900c49a6a33c039802423c74796130
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-10-15 22:55:09 +0530

    Power of 2

---
+++ java/src/main/java/problems/leetcode/top_interview_questions/easy/PowerOf2.java
@@ -0,0 +1,12 @@
+package problems.leetcode.top_interview_questions.easy;
+
+public class PowerOf2 {
+
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        long x = (long) n;
+        return (x & (-x)) == x;
+    }
+
+
+}
