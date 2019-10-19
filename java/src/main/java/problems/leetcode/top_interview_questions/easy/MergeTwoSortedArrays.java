b4ef6efee5c04341aef4505aa012de7af5010a77
Rajeev-Hegde <rajeevh.93@gmail.com>
2019-10-19 19:27:20 +0530

    Merge two sorted arrays efficient solution

--- java/src/main/java/problems/leetcode/top_interview_questions/easy/MergeTwoSortedArrays.java
+++ java/src/main/java/problems/leetcode/top_interview_questions/easy/MergeTwoSortedArrays.java
@@ -23,4 +23,29 @@ public class MergeTwoSortedArrays {
             nums1[i] = nums2[i-m];
         }
     }
+
+    public void mergeEfficient(int[] nums1, int m, int[] nums2, int n) {
+
+        if(n>0) {
+            int i = m + n - 1;
+
+            int j = n - 1;
+            int k= m-1;
+
+            while(i>=0 && j>=0 && k>=0){
+
+                if(nums2[j] >= nums1[k]){
+                    nums1[i--]= nums2[j--];
+                }
+                else
+                    nums1[i--]= nums1[k--];
+            }
+
+            while(j>=0){
+                nums1[i--]= nums2[j--];
+            }
+        }
+
+    }
+
 }
